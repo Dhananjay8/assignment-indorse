e30a336ff5372d4e4a809a56cbf0372bf30fa522
Dhananjay8 <dhananjaypatil070@gmail.com>
2020-09-05 15:28:23 +0530

    basuc code added

---
+++ set_env_vars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Core ENV Details
+export PA_PORT=8000
+
+# mysql main db
+export MYSQL_HOST='localhost'
+export MYSQL_USER='dhananjay'
+export MYSQL_PASSWORD='dhananjay'
+export MYSQL_DB_NAME='test'
+
+export API_DOMAIN='localhost:8000'
